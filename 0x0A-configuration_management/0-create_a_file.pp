4e82da9fe4ab14ee3690978c5588ec19e7d24800
kael1706 <cdcp-1795@hotmail.com>
2020-03-24 01:02:21 +0000

    a0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,9 @@
-#!/usr/bin/env bash
+# with poppet we can create a file in /tmp
+
+file { '/tmp/holberton':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
